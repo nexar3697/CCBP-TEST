60dbb7fbe67213f21d678e250f4ec19a547aa7a8
Naveen <naveen.run17@gmail.com>
2022-07-30 02:22:49 +0000

    Using date-fsn package

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const add = require("./calculator");
 console.log(add(6, 4));
 
+const path = require("path");
+const filePath = path.join("users/myprojecsts/daredevil.js");
+console.log(typeof filePath);
+
+//Date Module
+const addDays = require("date-fns/addDays");
+const format = require("date-fns/format");
+
+const dateFormat = format(new Date(2022, 03, 28), "yyyy-MM-dd");
+const result = addDays(new Date(2022, 03, 20), 8);
+console.log(dateFormat);
+console.log(result);
